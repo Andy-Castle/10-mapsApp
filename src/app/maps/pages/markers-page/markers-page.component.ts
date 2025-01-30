12d8100b86c3c06ed0a06af8b4e69204871ad5de
Andy Castle <masterchief_and@live.com>
2025-01-30 16:46:35 -0500

    Se hizo la prueba para poder mostrar un marcardor en el mapa, esa parte esta comentada y se creo la estrucutra para poder añadir un marcador

--- src/app/maps/pages/markers-page/markers-page.component.ts
+++ src/app/maps/pages/markers-page/markers-page.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { LngLat, Map, Marker } from 'mapbox-gl';
 
 @Component({
   templateUrl: './markers-page.component.html',
-  styles: [
-  ]
+  styleUrls: ['./markers-page.components.css'],
 })
-export class MarkersPageComponent {
+export class MarkersPageComponent implements AfterViewInit {
+  @ViewChild('map')
+  public divmap?: ElementRef;
 
+  public map?: Map;
+
+  public currentLngLat: LngLat = new LngLat(
+    -99.23120105306828,
+    21.840564007528073
+  );
+
+  ngAfterViewInit(): void {
+    if (!this.divmap) throw 'El elemento HTML no fue encontrado';
+
+    this.map = new Map({
+      container: this.divmap.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: this.currentLngLat, // starting position [lng, lat]
+      zoom: 10, // starting zoom
+    });
+
+    // const markerHtml = document.createElement('div');
+    // markerHtml.innerHTML = 'Andy';
+
+    // const marker = new Marker({
+    //   // color: 'red',
+    //   element: markerHtml,
+    // })
+    //   .setLngLat(this.currentLngLat)
+    //   .addTo(this.map!);
+  }
 }
