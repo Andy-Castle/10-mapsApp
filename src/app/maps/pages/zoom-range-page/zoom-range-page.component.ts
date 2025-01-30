564f2bbd87b392ccb0d13c43b6483dba3a49725b
Andy Castle <masterchief_and@live.com>
2025-01-30 16:31:42 -0500

    Se obtuvo las coordenadas para mostrarlas en pantalla y se destruyo los listeners para no afectar el rendimiento

--- src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -1,17 +1,28 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
-import { Map } from 'mapbox-gl';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnDestroy,
+  ViewChild,
+} from '@angular/core';
+import { LngLat, Map } from 'mapbox-gl';
 
 @Component({
   templateUrl: './zoom-range-page.component.html',
   styleUrls: ['./zoom-range-page.component.css'],
 })
-export class ZoomRangePageComponent implements AfterViewInit {
+export class ZoomRangePageComponent implements AfterViewInit, OnDestroy {
   @ViewChild('map')
   public divmap?: ElementRef;
 
   public zoom: number = 10;
 
   public map?: Map;
+
+  public currentLngLat: LngLat = new LngLat(
+    -99.23120105306828,
+    21.840564007528073
+  );
 
   ngAfterViewInit(): void {
     console.log(this.divmap);
@@ -21,11 +32,18 @@ export class ZoomRangePageComponent implements AfterViewInit {
     this.map = new Map({
       container: this.divmap.nativeElement, // container ID
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
-      center: [-74.5, 40], // starting position [lng, lat]
+      center: this.currentLngLat, // starting position [lng, lat]
       zoom: this.zoom, // starting zoom
     });
 
     this.mapListeners();
+  }
+
+  ngOnDestroy(): void {
+    // this.map?.off('move', () => {
+    //   this.mapListeners()
+    // });
+    this.map?.remove();
   }
 
   mapListeners() {
@@ -46,6 +64,13 @@ export class ZoomRangePageComponent implements AfterViewInit {
       // console.log(event);
       if (this.map!.getZoom() > 2) return;
       this.map!.zoomTo(2);
+    });
+
+    this.map.on('move', (event) => {
+      this.currentLngLat = this.map!.getCenter();
+      // console.log(this.currentLngLat);
+      const { lat, lng } = this.currentLngLat;
+      // console.log(lat, lng);
     });
   }
 
