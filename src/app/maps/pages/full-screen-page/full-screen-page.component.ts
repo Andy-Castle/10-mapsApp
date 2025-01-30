a705978fabb50b705847d99eea2e25b7de913d73
Andy Castle <masterchief_and@live.com>
2025-01-30 15:32:50 -0500

    se mostro el mapa de mapbox en el componente

--- src/app/maps/pages/full-screen-page/full-screen-page.component.ts
+++ src/app/maps/pages/full-screen-page/full-screen-page.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+
+import * as mapboxgl from 'mapbox-gl';
+import { environment } from 'src/environments/environment';
+
+(mapboxgl as any).accessToken = environment.mapbox_key;
 
 @Component({
   templateUrl: './full-screen-page.component.html',
-  styles: [
-  ]
+  styleUrls: ['./full-screen-page.component.css'],
 })
-export class FullScreenPageComponent {
-
+export class FullScreenPageComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    const map = new mapboxgl.Map({
+      container: 'map', // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: 9, // starting zoom
+    });
+  }
 }
