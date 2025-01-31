6599cb77eb2f958f3c29a7d8f71717bad9537f89
Andy Castle <masterchief_and@live.com>
2025-01-31 11:12:11 -0500

    Se estrcuturo el componente del mini-map y se implemento en el

--- src/app/maps/components/mini-map/mini-map.component.ts
+++ src/app/maps/components/mini-map/mini-map.component.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Input,
+  ViewChild,
+} from '@angular/core';
+import { Map, Marker } from 'mapbox-gl';
 
 @Component({
-  selector: 'app-mini-map',
+  selector: 'map-mini-map',
   templateUrl: './mini-map.component.html',
-  styles: [
-  ]
+  styleUrls: ['./mini-map.component.css'],
 })
-export class MiniMapComponent {
+export class MiniMapComponent implements AfterViewInit {
+  @Input() lngLat?: [number, number];
 
+  @ViewChild('map')
+  public divmap?: ElementRef;
+
+  ngAfterViewInit(): void {
+    if (!this.divmap) throw 'El elemento HTML no fue encontrado';
+    if (!this.lngLat) throw "LngLat can't be null";
+
+    const color = '#xxxxxx'.replace(/x/g, (y) =>
+      ((Math.random() * 16) | 0).toString(16)
+    );
+
+    const map = new Map({
+      container: this.divmap?.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: this.lngLat, // starting position [lng, lat]
+      zoom: 15, // starting zoom
+      interactive: false,
+    });
+
+    const marker = new Marker({
+      color: color,
+    })
+      .setLngLat(this.lngLat)
+      .addTo(map);
+  }
 }
